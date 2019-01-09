8ce00324acb6e0ae25b9d54f2ff798fe74812f25
emgalam21 <46469217+emgalam21@users.noreply.github.com>
2019-01-09 19:35:28 +0700

    Add files via upload

---
+++ baitap1 buoi 1-2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int	a;
+	int b;
+	int c;
+	int d;
+	double result;
+	
+	cout <<"input a , b , c";
+	cin>>a>>b>>c;
+	d=a+b+c;
+	result = d/3.0;
+	cout<<"Avg = "<<result;
+	return 0;
+}
